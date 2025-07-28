86e33db85332fdf33b9b40a5db3b91aae5ccee3d
Visalakshi11 <visalakshipl11@gmail.com>
2025-07-28 18:09:51 +0530

    Add files via upload

---
+++ java c2c/personDemo.java
@@ -0,0 +1,15 @@
+
+public class personDemo{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		person per = new person();
+		per.setName("Akash");
+		per.setGender("Male");
+		per.setAge(20);
+		per.setIncome(30000);
+		
+        System.out.println(per);
+	}
+
+}
